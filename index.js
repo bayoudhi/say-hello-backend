e0faf38b18385bfc51581a6514698d22929bf5f1
Hamza Mehri <azizelmehri@gmail.com>
2020-12-15 16:48:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const epxress = require("express");
+const cors = require("cors");
+
+const app = epxress();
+app.use(cors());
+
+// Constants
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+// App
+app.get("/", (req, res) => {
+  if (!req.query.name) {
+    res.status(400).send({
+      error: "querystring name is missing",
+    });
+  }
+
+  res.status(200).send({
+    message: `Hello  ${req.query.name || "Stranger"}`,
+  });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
